e809712c203296d8ccfb1d0b36e1f0af51905431
Martin Buberl <hello@martinbuberl.com>
2017-03-25 22:26:21 +0100

    Changed implementation to config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+"""Configuration settings"""
+import os
+
+GITHUB_USERNAME = os.getenv("GITHUB_USERNAME") or "YOUR_GITHUB_USERNAME"
+GITHUB_PASSWORD = os.getenv("GITHUB_PASSWORD") or "YOUR_GITHUB_PASSWORD"
