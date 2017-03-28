5955d56809c41d59f8b84c5bdf6b42b1b4849b0c
Martin Buberl <hello@martinbuberl.com>
2017-03-28 23:13:21 +0200

    Added file check

--- repolint.py
+++ repolint.py
@@ -7,7 +7,14 @@ def repolint(repo):
 
     branches = repo.get_branches()
     for branch in branches:
-        print "+ " + branch.name
+        if branch.name == "master":
+            for file in repo.get_dir_contents("/"):
+                print file
+                # check for
+                # - README
+                # - LICENSE
+                # - .gitignore
+                # - .editorconfig
 
     pulls = repo.get_pulls()
     for pr in pulls:
@@ -15,3 +22,21 @@ def repolint(repo):
         pr_age = datetime.now() - pr.created_at
         if pr_age.days >= 7: # old pull requests
             cprint(repo.name + " " + pr.head.label[11:], "yellow")
+
+# # active branches
+# - merged
+# - un-merged
+# # stale branches
+# - merged
+# - un-merged
+# # open pull requests
+# - proportion open:closed pull requests
+# # open issues
+# - proportion open:closed issues
+# CI
+# Node.js
+# - npm
+# - package.json
+# Python
+# - pypi
+# master branch protection
