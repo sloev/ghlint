1099277199ae2b1705f044227abe897944274951
martinbuberl <mbl@trustpilot.com>
2017-03-30 17:36:25 +0200

    Added ghlint find path

--- config.py
+++ config.py
@@ -3,3 +3,14 @@ import os
 
 GITHUB_USERNAME = os.getenv("GITHUB_USERNAME") or "YOUR_GITHUB_USERNAME"
 GITHUB_PASSWORD = os.getenv("GITHUB_PASSWORD") or "YOUR_GITHUB_PASSWORD"
+
+def find_ghlintrc():
+    """search the ghlintrc file and return its path"""
+    ghlintrc = None
+
+    if os.path.exists('.ghlintrc'):
+        ghlintrc = os.path.abspath('.ghlintrc')
+
+    return ghlintrc
+
+GHLINTRC = find_ghlintrc()
