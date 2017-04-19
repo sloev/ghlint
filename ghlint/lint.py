081fcd0b14046f1014b5aa1eed387aa69a60325c
Martin Buberl <hello@martinbuberl.com>
2017-04-20 00:11:48 +0200

    Wrapped in def

--- ghlint/lint.py
+++ ghlint/lint.py
@@ -1,9 +1,10 @@
 """Basic console implementation"""
-import config
-import repolint
+from . import config
+from . import repolint
 from github import Github
 
-GITHUB = Github(config.GITHUB_USERNAME, config.GITHUB_PASSWORD)
+def lint():
+    GITHUB = Github(config.GITHUB_USERNAME, config.GITHUB_PASSWORD)
 
-for repo in GITHUB.get_user().get_repos():
-    repolint.repolint(repo)
+    for repo in GITHUB.get_user().get_repos():
+        repolint.repolint(repo)
