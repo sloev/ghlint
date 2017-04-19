c07ac3feec94a82fbc977fdf69d28ff35e3b82de
Martin Buberl <hello@martinbuberl.com>
2017-04-20 00:23:32 +0200

    Fixed invalid const name warnings

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup, find_packages
 
 
 with open('README.md') as f:
-    readme = f.read()
+    README = f.read()
 
 with open('LICENSE') as f:
-    license = f.read()
+    LICENSE = f.read()
 
 setup(
     name='ghlint',
     version='0.1.0',
     description='foo',
-    long_description=readme,
+    long_description=README,
     author='Martin Buberl',
     author_email='hello@martinbuberl.com',
     url='https://github.com/martinbuberl/ghlint',
-    license=license,
+    license=LICENSE,
     packages=find_packages(exclude=('tests', 'docs'))
 )
