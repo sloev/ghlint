9549293958b55fc2589ed905ea3407748d62eee7
martinbuberl <mbl@trustpilot.com>
2017-04-27 16:58:52 +0200

    Added simple output

--- ghlint/main.py
+++ ghlint/main.py
@@ -4,6 +4,11 @@ from github import Github
 
 
 def lint():
+    ghlintr = config.read_ghlintrc()
+    print ghlintr.sections()
+    print ghlintr["RULES"]
+    print ghlintr["RULES"]["editorconfig"]
+
     GITHUB = Github(config.GITHUB_USERNAME, config.GITHUB_PASSWORD)
 
     for repo in GITHUB.get_user().get_repos():
