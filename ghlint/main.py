d3a871e4e57d4aae15e45c453a277dd9dd523c6c
Martin Buberl <hello@martinbuberl.com>
2017-04-30 10:52:27 +0200

    Merged files

--- ghlint/main.py
+++ ghlint/main.py
@@ -1,5 +1,5 @@
 import config
-import repolint
+from datetime import datetime
 from github import Github
 
 
@@ -13,6 +13,33 @@ def lint():
 
     for repo in github.get_user().get_repos():
         repolint.repolint(repo)
+
+def repolint(repo):
+    print repo.name
+
+    branches = repo.get_branches()
+    for branch in branches:
+        if branch.name == "master":
+            has_editorconfig = False
+            for file_in_root in repo.get_dir_contents("/"):
+                if file_in_root.path == ".editorconfig":
+                    has_editorconfig = True
+                if file_in_root.path == ".gitignore":
+                    has_gitignore = True
+                if file_in_root.path == "LICENSE": # LICENSE.txt (or LICENSE.md
+                    has_license = True
+                if file_in_root.path == "README":
+                    has_readme = True
+            if has_editorconfig != True:
+                print "No .editorconfig"
+
+    pulls = repo.get_pulls()
+    for pull in pulls:
+        print "* " + pull.head.label[11:]
+        pr_age = datetime.now() - pull.created_at
+        if pr_age.days >= 7: # old pull requests
+            print repo.name + " " + pull.head.label[11:]
+
 
 if __name__ == "__main__":
     lint()
