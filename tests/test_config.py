22c043a54f95e0fd97fb5063c0a870242e66d4b7
martinbuberl <mbl@trustpilot.com>
2017-06-26 17:06:54 +0200

    Added tests for config

---
+++ tests/test_config.py
@@ -0,0 +1,22 @@
+import os
+import unittest
+from ghlint import config
+
+
+class TestConfig(unittest.TestCase):
+
+    def test_ghlintrc_path(self):
+        ghlintrc_path = config.ghlintrc_path()
+        expected_path = os.getcwd() + '/.ghlintrc'
+
+        self.assertEqual(ghlintrc_path, expected_path)
+
+    def test_settings_defaults(self):
+        settings = config.settings()
+
+        self.assertEqual(settings["account-type"], "user")
+        self.assertEqual(settings["repo-type"], "owner")
+
+
+if __name__ == '__main__':
+    unittest.main()
