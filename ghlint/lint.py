730d27672598a09f6fd25ba5851aada7904ab3a1
martinbuberl <mbl@trustpilot.com>
2017-06-26 17:48:47 +0200

    Added first version of old pulls rule

--- ghlint/lint.py
+++ ghlint/lint.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from datetime import datetime
 import config
 from termcolor import cprint
 from github.GithubException import UnknownObjectException
@@ -6,7 +7,7 @@ from github.GithubException import UnknownObjectException
 
 def lint(repo):
     # this is for debugging only
-    #if repo.name != "boilerplate-api":
+    #if repo.name != "TeamPilgrim":
     #    return
 
     branches = repo.get_branches()
@@ -27,6 +28,7 @@ def lint(repo):
             rule_editorconfig(repo, ghlintrc)
             rule_gitignore(repo, ghlintrc)
             rule_protection(repo, ghlintrc)
+            rule_old_pulls(repo, ghlintrc)
 
 def get_file_found(repo, file_name):
     root = "/"
@@ -54,35 +56,79 @@ def print_message(condition, rule, message):
             cprint(message, "red")
 
 def rule_ghlintrc(repo, ghlintrc):
-    condition = get_file_found(repo, ".ghlintrc")
     rule = get_rule_value(repo, ghlintrc, "ghlintrc")
+    if rule == "off":
+        return
+
+    condition = get_file_found(repo, ".ghlintrc")
     message = "File .ghlintrc not found"
     print_message(condition, rule, message)
 
 def rule_editorconfig(repo, ghlintrc):
-    condition = get_file_found(repo, ".editorconfig")
     rule = get_rule_value(repo, ghlintrc, "editorconfig")
+    if rule == "off":
+        return
+
+    condition = get_file_found(repo, ".editorconfig")
     message = "File .editorconfig not found"
     print_message(condition, rule, message)
 
 def rule_gitignore(repo, ghlintrc):
-    condition = get_file_found(repo, ".gitignore")
     rule = get_rule_value(repo, ghlintrc, "gitignore")
+    if rule == "off":
+        return
+
+    condition = get_file_found(repo, ".gitignore")
     message = "File .gitignore not found"
     print_message(condition, rule, message)
 
 def rule_protection(repo, ghlintrc):
+    rule = get_rule_value(repo, ghlintrc, "protection")
+    if rule == "off":
+        return
+
     branch = repo.get_protected_branch(repo.default_branch)
     condition = branch.protected
-    rule = get_rule_value(repo, ghlintrc, "protection")
     message = "Branch '" + repo.default_branch + "' not protected"
     print_message(condition, rule, message)
 
+def rule_old_pulls(repo, ghlintrc):
+    rule = get_rule_value(repo, ghlintrc, "rule_old_pulls")
+    if rule == "off":
+        return
 
-#def foo(repo, ghlintrc):
-#    pulls = repo.get_pulls()
-#    for pull in pulls:
-#        print("* " + pull.head.label[11:])
-#        pr_age = datetime.now() - pull.created_at
-#        if pr_age.days >= 7: # old pull requests
-#            print(repo.name + " " + pull.head.label[11:])
+    pulls = repo.get_pulls()
+    for pull in pulls:
+        pr_age = datetime.now() - pull.created_at
+        pr_max_age = int(get_rule_value(repo, ghlintrc, "rule_old_pulls_max_age"))
+        if pr_age.days >= pr_max_age and pull.state == "open":
+            print("title: " + pull.title)
+            print("url: " + pull.url)
+            print("number: " + str(pull.number))
+            print(pull.base)
+            print(pull.head.label)
+            print(pull.head.ref)
+            print(repo.name + " " + pull.head.label[11:])
+
+# - description
+# - CONTRIBUTING.md
+# # active branches
+# - merged
+# - un-merged
+# # stale branches
+# - merged
+# - un-merged
+# # open pull requests
+# - proportion open:closed pull requests
+# # open issues
+# - proportion open:closed issues
+# license detection
+# - https://developer.github.com/v3/licenses/#get-a-repositorys-license
+# CI
+# Node.js
+# - npm
+# - package.json
+# Python
+# - pypi
+# master branch protection
+
