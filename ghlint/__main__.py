ca4ca79376f036052a80219aaf08ceb0ea7c5141
martinbuberl <hello@martinbuberl.com>
2017-08-22 16:13:46 +0200

    Workaround for absolute path not found

--- ghlint/__main__.py
+++ ghlint/__main__.py
@@ -1,5 +1,14 @@
-from ghlint import config
-from ghlint import lint
+import sys
+from inspect import getsourcefile
+from os import path
+
+
+CURRENT_DIR = path.dirname(path.abspath(getsourcefile(lambda: 0)))
+sys.path.insert(0, CURRENT_DIR[: CURRENT_DIR.rfind(path.sep)])
+
+
+from ghlint import config # pylint: disable=wrong-import-position
+from ghlint import lint # pylint: disable=wrong-import-position
 
 
 def main():
--- ghlint/cli.py
+++ ghlint/cli.py
@@ -1,6 +1,15 @@
-import click
-from . import config
-from . import lint
+import sys
+from inspect import getsourcefile
+from os import path
+
+
+CURRENT_DIR = path.dirname(path.abspath(getsourcefile(lambda: 0)))
+sys.path.insert(0, CURRENT_DIR[: CURRENT_DIR.rfind(path.sep)])
+
+
+import click # pylint: disable=wrong-import-position
+from ghlint import config # pylint: disable=wrong-import-position
+from ghlint import lint # pylint: disable=wrong-import-position
 
 
 @click.command()
